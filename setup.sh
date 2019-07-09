82e57a172ab9077607ae294c0b359e33fffe4a52
hissingshark <git@hissingshark.co.uk>
2019-07-09 22:13:19 +0100

    Condense into one script file - no need for sourcing at this size/complexity

--- setup.sh
+++ setup.sh
@@ -1,5 +1,85 @@
 #!/bin/bash
 
+# load variables and functions
+. ./resources/data.sh
+
+#############
+# constants #
+#############
+
+LOGO='retrOSMCmk2'
+DIALOG_OK=0
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+
+#############
+# FUNCTIONS #
+#############
+
+# perform initial installation
+function firstTimeSetup() {
+    # get dependancies
+    echo  "\nInstalling required dependancies..."
+    apt-get install -y git dialog || { echo "FAILED!"; exit 1; }
+    echo -e "SUCCESS!\n"
+
+    # get RetroPie-Setup
+    echo "Installing RetroPie-Setup..."
+#    git submodule add https://github.com/RetroPie/RetroPie-Setup.git || { echo "FAILED!"; exit; }
+    git -C submodule/ clone https://github.com/RetroPie/RetroPie-Setup.git || { echo "FAILED!"; exit 1; }
+    echo "SUCCESS!\n"
+
+    # install EmulationStation launch service
+    echo "Installing emulationstation.service..."
+    cp scripts/emulationstation.service /etc/systemd/system/ || { echo "FAILED!"; exit 1; }
+    systemctl daemon-reload || { echo "FAILED!"; exit 1; }
+    echo -e "SUCCESS!\n"
+
+    # install scripts into RetroPie directory
+    # create it first if it doesn't exist - RetroPie-Setup wont remove it at install/update
+    if [[ ! -d /home/osmc/RetroPie/scripts ]]; then
+        mkdir -p /home/osmc/RetroPie/scripts
+    fi
+    cp scripts/launcher.sh scripts/tvservice-shim /home/osmc/RetroPie/scripts
+
+    # not do this again
+    first_run=0
+
+    writeData
+    return 0
+}
+
+# re-patch Retropie after an update
+function patchRetroPie() {
+    # PATCH 1
+    # encapsulate the RetroPie update function with our own, so we get to repatch after they update
+    # rename the original function away
+    sed -i '/function updatescript_setup()/s/updatescript_setup/updatescript_setup_original/' submodule/RetroPie-Setup/scriptmodules/admin/setup.sh
+    # append our wrapper function
+    cat resources/updatescript_setup.sh >> submodule/RetroPie-Setup/scriptmodules/admin/setup.sh
+
+    # PATCH 2
+    # use tvservice-shim instead of the real thing
+    if [[ -e  "/opt/retropie/supplementary/runcommand/runcommand.sh" ]]; then
+        # installed version patched in place
+        sed -i '/TVSERVICE=/s/.*/TVSERVICE=\"\/home\/osmc\/RetroPie\/scripts\/tvservice-shim\"/' /opt/retropie/supplementary/runcommand/runcommand.sh
+    fi
+    # patch the resource file regardless as there may be a re-install from there later
+    sed -i '/TVSERVICE=/s/.*/TVSERVICE=\"\/home\/osmc\/RetroPie\/scripts\/tvservice-shim\"/' submodule/RetroPie-Setup/scriptmodules/supplementary/runcommand/runcommand.sh
+
+    # PATCH 3
+    # make binaries available for Vero4K
+    sed -i '/__binary_host="/s/.*/__binary_host="hissingshark.co.uk"/' submodule/RetroPie-Setup/scriptmodules/system.sh
+    sed -i '/__has_binaries=/s/0/1/' submodule/RetroPie-Setup/scriptmodules/system.sh
+
+    return 0
+}
+
+
+#########################
+# EXECUTION STARTS HERE #
+#########################
+
 # check we are running as root for all of the install work
 if [ "$EUID" -ne 0 ]; then
     echo -e "\n***\nPlease run as sudo.\nThis is needed for installing any dependencies as we go and for running RetroPie-Setup.\n***"
@@ -8,16 +88,6 @@ fi
 
 # all operations performed relative to this script
 pushd $(dirname "${BASH_SOURCE[0]}") >/dev/null
-
-# load variables and functions
-. ./resources/data.sh
-. ./resources/functions.sh
-
-# constants
-LOGO='retrOSMCmk2'
-DIALOG_OK=0
-DIALOG_CANCEL=1
-DIALOG_ESC=255
 
 # perform initial setup if this is the 1st run
 if [[ first_run -eq 1 ]]; then
