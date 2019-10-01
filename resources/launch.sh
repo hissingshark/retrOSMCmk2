d5c675bd3892f3adca53b6853fee06e23f6bca3e
hissingshark <git@hissingshark.co.uk>
2019-10-01 04:06:28 +0100

    Set frame buffer size at launch

--- resources/launch.sh
+++ resources/launch.sh
@@ -8,6 +8,7 @@ function cleanup()
 
 systemctl stop mediacenter
 sudo sh -c 'echo 1080p50hz > /sys/class/display/mode'
+sudo sh -c 'fbset -g 1920 1080 1920 2160 32'
 chvt 1
 trap cleanup EXIT
 sudo -u osmc /usr/bin/emulationstation
