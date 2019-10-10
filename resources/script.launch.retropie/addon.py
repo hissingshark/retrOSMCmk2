e6f9dd4dba2241fa46fbd9ef65e9f091906124dd
hissingshark <git@hissingshark.co.uk>
2019-10-10 22:18:51 +0100

    Prevent TV/AVR shutdown when starting RetroPie
    If Kodi is configured to put TV/AVR into standby via CEC at shutdown, then this triggers when stopping temporarily to start RetroPie.
    The workaround is briefly running cec-client to interrupt it.
    
    May require refinement of the timings or shutdown of the CEC adapter altogether by killing the client before the addon runs.

--- resources/script.launch.retropie/addon.py
+++ resources/script.launch.retropie/addon.py
@@ -1,2 +1,14 @@
-import os
+import os, subprocess, time
+
+# Interrupt Kodi sending any "standby" messages to a TV/AVR when we stop mediacenter.service later
+cecc = subprocess.Popen("/usr/osmc/bin/cec-client", stdout=subprocess.PIPE, universal_newlines=True)
+time.sleep(1)
+
+# launch RetroPie
 os.system('systemctl start emulationstation.service')
+time.sleep(1)
+
+# stop cec-client
+cecc.terminate()
+cecc.communicate()
+
