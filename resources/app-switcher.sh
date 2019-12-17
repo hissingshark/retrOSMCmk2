19a9a654e7b5cdc34f00a769557bbebfe2d6b623
hissingshark <git@hissingshark.co.uk>
2019-12-17 11:34:18 +0000

    4K fix optional and improved PA handling

--- resources/app-switcher.sh
+++ resources/app-switcher.sh
@@ -13,16 +13,21 @@ while true; do
     # supplied parameters
     opts=( $msg )
     DESTINATION="${opts[0]}" # es = emulationstation, mc = mediacenter
-    MODE="${opts[1]}" # fast, slow or pre(load)
+    MODE="${opts[1]}" # fast or slow
+    4KFIX="${opts[2]}" # true or false - some 4K TVs suffer flickering, distortion etc
 
-    # group process IDs if applications pre-loaded
+    # group process IDs if applications are pre-loaded
     ES_GPID=$(ps xao pgid,comm | grep -m 1 "emulationstatio" | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f1 | tr -d ' ')
     MC_GPID=$(ps xao pgid,comm | grep -m 1 "kodi.bin" | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f1 | tr -d ' ')
 
     if [[ "$DESTINATION" == "es" ]]; then
+      if [[ "$4KFIX" == "true" ]]; then
+        sudo sh -c 'echo 1080p50hz > /sys/class/display/mode'
+        sudo sh -c 'fbset -g 1920 1080 1920 2160 32'
+      fi
+
+# Still needed? Wasn't part of the manual tests...
       sudo chvt 1
-      sudo sh -c 'echo 1080p50hz > /sys/class/display/mode'
-      sudo sh -c 'fbset -g 1920 1080 1920 2160 32'
 
       if [[ "$MODE" == "slow" ]]; then
         systemctl stop mediacenter
@@ -52,7 +57,10 @@ while true; do
         continue
       fi
 
+      sudo -u osmc pactl --server="$PA_SERVER" suspend-sink alsa_output.platform-aml_m8_snd.46.analog-stereo 1
+
       systemctl stop cec-exit
+#      systemctl stop evtest-exit
 
       if [[ -z "$MC_GPID" ]]; then
         systemctl start mediacenter
