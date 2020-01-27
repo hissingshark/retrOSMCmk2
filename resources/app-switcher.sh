e504a014e101d96663f6fff72b0cf7ccd917da2e
hissingshark <git@hissingshark.co.uk>
2020-01-27 21:12:29 +0000

    New resources for slot manager feature

--- resources/app-switcher.sh
+++ resources/app-switcher.sh
@@ -1,85 +1,198 @@
 #!/bin/bash
 
-# switches between Kodi and RetroPie, as instructed over a FIFO
+# this script switches between Kodi and RetroPie, as instructed over a FIFO
 # slow mode stops and starts services in turn
-# fast mode halt and resumes processes via "kill STOP/CONT"
+# fast mode halts and resumes processes via "kill STOP/CONT"
+
+#LOG=/home/osmc/debug.log
+#echo  "Log Starts.." > $LOG
+#echo  "" >> $LOG
+
+#############
+# FUNCTIONS #
+#############
+
+# takes an element number and a reference to an array - removes that element
+function cutArray() {
+  local tocut=$1
+  local -n array=$2
+  local tmp=()
+
+  for ((line=0; line<${#array[@]}; line++)); do
+    [[ ! "$line" == "$tocut" ]] && tmp+=("${array[$line]}")
+  done
+
+  array=("${tmp[@]}")
+}
+
+
+
+##############
+#  VARIABLES #
+##############
+
+ACTIVE_SESSION=0 # active session slot (not an index as 0 is Kodi)
+# list of each session's...
+PGIDS=(null) # process-group ID
+PLATFORMS=(OSMC) # currently emulated platform
+ROMS=(Kodi) # ROM in play
+TARGETS=(null) # systemd service target
+# initialised with Kodi always as the first slot - cuts down on variable arithmetic later
 
 FIFO=/tmp/app-switcher.fifo
 
+
+
+#########################
+# EXECUTION BEGINS HERE #
+#########################
+
+# setup FIFO for communication
 if [[ ! -p $FIFO ]]; then
   sudo -u osmc mkfifo $FIFO
 fi
 
-
 while true; do
   if read msg <$FIFO; then
-    # supplied parameters
+    # extract supplied parameters
     opts=( $msg )
-    DESTINATION="${opts[0]}" # es = emulationstation, mc = mediacenter
-    MODE="${opts[1]}" # fast or slow
-    4KFIX="${opts[2]}" # true or false - some 4K TVs suffer flickering, distortion etc
-
-    # group process IDs if applications are pre-loaded
-    ES_GPID=$(ps xao pgid,comm | grep -m 1 "emulationstatio" | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f1 | tr -d ' ')
-    MC_GPID=$(ps xao pgid,comm | grep -m 1 "kodi.bin" | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f1 | tr -d ' ')
+    MODE="${opts[0]}" # dumping the arrays, updating the arrays or switching application
 
     # socket to pulseaudioserver if running
     PA_SERVER=$(ls /home/osmc/.config/pulse/*-runtime/native)
 
-    if [[ "$DESTINATION" == "es" ]]; then
-      if [[ "$4KFIX" == "true" ]]; then
-        sudo sh -c 'echo 1080p50hz > /sys/class/display/mode'
-        sudo sh -c 'fbset -g 1920 1080 1920 2160 32'
-      fi
+    # act on requested mode
+    if [[ "$MODE" == "dump" ]]; then
+      # send : delimited list of...
+      DUMP=$(( ${#PGIDS[@]} - 1 )) # how many slots
+      for ((num=1; num<${#PGIDS[@]}; num++)); do # followed by the contents
+# TODO we only get the first word of ROM titles containing spaces
+        DUMP+=":${PLATFORMS[$num]}:"
+# trying with roms in seperate quotes in case its only sending the first ekement of the array
+        DUMP+="${ROMS[$num]}"
+      done
+      echo "$DUMP" > "$FIFO"
 
-      if [[ "$MODE" == "slow" ]]; then
-        systemctl stop mediacenter
-      elif [[ "$MODE" == "fast" ]]; then
-        sudo kill -STOP "-$MC_GPID"
-        # console normally unbound when Kodi exits - without it there's no runcommand.sh console menu
-        echo 1 >/sys/class/vtconsole/vtcon1/bind
+    elif [[ "$MODE" == "tty" ]]; then
+      # share session TTY for use in runcommand.sh (same as service target num)
+      echo "${TARGETS[$ACTIVE_SESSION]}" > "$FIFO"
+
+    elif [[ "$MODE" == "update" ]]; then
+      PLATFORMS[$ACTIVE_SESSION]="${opts[1]}"
+#      ROMS[$ACTIVE_SESSION]="${opts[2]}"
+      shift 1
+      ROMS[$ACTIVE_SESSION]="${opts[@]}"
+
+    elif [[ "$MODE" == "delete" ]]; then
+      ACTIVE_SESSION="${opts[1]}"
+      systemctl stop emulationstation@${TARGETS[$ACTIVE_SESSION]}.service
+#      sudo kill -KILL "-${PGIDS[$ACTIVE_SESSION]}"
+      cutArray $ACTIVE_SESSION PGIDS
+      cutArray $ACTIVE_SESSION PLATFORMS
+      cutArray $ACTIVE_SESSION ROMS
+      cutArray $ACTIVE_SESSION TARGETS
+
+    elif [[ "$MODE" == "switch" ]]; then
+      DESTINATION="${opts[1]}" # es = emulationstation, mc = mediacenter
+      SPEED="${opts[2]}" # fast or slow
+      REQUESTED_SESSION="${opts[3]}" # requested session to rejoin
+      4KFIX="${opts[4]}" # true or false - some 4K TVs suffer flickering, distortion etc
+
+
+      if [[ "$DESTINATION" == "es" ]]; then
+        if [[ "$4KFIX" == "true" ]]; then
+          sudo sh -c 'echo 1080p50hz > /sys/class/display/mode'
+          sudo sh -c 'fbset -g 1920 1080 1920 2160 32'
+        fi
+
+        # shutdown or halt Kodi processes
+        if [[ "$SPEED" == "slow" ]]; then
+          systemctl stop mediacenter
+          ${PGIDS[0]}="null"
+        elif [[ "$SPEED" == "fast" ]]; then
+          PGIDS[0]=$(ps xao pgid,comm | grep -m 1 "kodi.bin" | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f1 | tr -d ' ')
+          sudo kill -STOP "-${PGIDS[0]}"
+          # console normally unbound when Kodi exits - without it there's no runcommand.sh console menu
+          echo 1 >/sys/class/vtconsole/vtcon1/bind
+        else
+          continue
+        fi
+
+          # re-enable ALSA sink on the PA server
+          sudo -u osmc pactl --server="$PA_SERVER" suspend-sink alsa_output.platform-aml_m8_snd.46.analog-stereo 0
+
+        # start a new session or...
+        if [[ "$REQUESTED_SESSION" == 0 ]]; then
+          # retroarch cores must use SDL2 for audio, else the pulseaudio setup leads to severe distortion
+          sed -i '/audio_driver =/c\audio_driver = sdl2' /opt/retropie/configs/all/retroarch.cfg
+
+          # current session slot also the latest
+          ACTIVE_SESSION=${#PGIDS[@]}
+          # find next available target number to start the service unit
+          for ((tgt=1; tgt>0; tgt++)); do
+            if [[ ! ${TARGETS[@]} =~ $tgt ]]; then
+              TARGETS+=($tgt)
+              tgt=-1
+            fi
+          done
+          systemctl start emulationstation@${TARGETS[$ACTIVE_SESSION]}.service
+          sleep 2
+
+          # create list of unique PGIDS (in practice just appends the newest one as the active session)
+          for PGID in $(ps xao pgid,comm | grep "emulationstatio" | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f1 | tr -d ' '); do
+            if [[ ! "${PGIDS[@]}" =~ "$PGID" ]]; then
+              PGIDS+=($PGID)
+              PLATFORMS+=("EmulationStation")
+              ROMS+=("N/A")
+            fi
+          done
+
+        # ...or continue selected halted processes
+        else
+          ACTIVE_SESSION=$REQUESTED_SESSION
+          # minimise any audio crackle on resuming PA
+          sleep 0.5
+          sudo kill -CONT "-${PGIDS[$REQUESTED_SESSION]}"
+        fi
+
+      elif [[ "$DESTINATION" == "mc" ]]; then
+        if [[ "$SPEED" == "slow" ]]; then
+          systemctl stop emulationstation@${TARGETS[$ACTIVE_SESSION]}.service
+          cutArray $ACTIVE_SESSION PGIDS
+          cutArray $ACTIVE_SESSION PLATFORMS
+          cutArray $ACTIVE_SESSION ROMS
+          cutArray $ACTIVE_SESSION TARGETS
+        elif [[ "$SPEED" == "fast" ]]; then
+          sudo kill -STOP "-${PGIDS[$ACTIVE_SESSION]}"
+          # restore the console binding for Kodi
+          echo 0 >/sys/class/vtconsole/vtcon1/bind
+        else
+          continue
+        fi
+        ACTIVE_SESSION=0
+
+        # disconnects emulators from the audio device to avoid blocking Kodi from it
+        sudo -u osmc pactl --server="$PA_SERVER" suspend-sink alsa_output.platform-aml_m8_snd.46.analog-stereo 1
+
+        # exit methods no longer required
+        systemctl stop cec-exit
+        systemctl stop evdev-exit
+
+        # start fresh Kodi session or restart the halted one
+        if [[ "${PGIDS[0]}" == "null" ]]; then
+          systemctl start mediacenter
+        else
+          sudo kill -CONT "-${PGIDS[0]}"
+        fi
+
       else
+        # bad destination requested
         continue
-      fi
-
-      if [[ -z "$ES_GPID" ]]; then
-        # retroarch cores must use SDL2 for audio, else the pulseaudio setup leads to severe distortion
-        sed -i '/audio_driver =/c\audio_driver = sdl2' /opt/retropie/configs/all/retroarch.cfg
-        systemctl start emulationstation
-      else
-        sudo -u osmc pactl --server="$PA_SERVER" suspend-sink alsa_output.platform-aml_m8_snd.46.analog-stereo 0
-        # minimise any audio crackle on resuming PA
-        sleep 0.5
-        sudo kill -CONT "-$ES_GPID"
-      fi
-
-    elif [[ "$DESTINATION" == "mc" ]]; then
-      if [[ "$MODE" == "slow" ]]; then
-        systemctl stop emulationstation
-      elif [[ "$MODE" == "fast" ]]; then
-        sudo kill -STOP "-$ES_GPID"
-        # restore the console binding for Kodi
-        echo 0 >/sys/class/vtconsole/vtcon1/bind
-      else
-        continue
-      fi
-
-      # disconnects emulators from the audio device to avoid blocking Kodi from it
-      sudo -u osmc pactl --server="$PA_SERVER" suspend-sink alsa_output.platform-aml_m8_snd.46.analog-stereo 1
-
-      # exit methods no longer required
-      systemctl stop cec-exit
-      systemctl stop evdev-exit
-
-      if [[ -z "$MC_GPID" ]]; then
-        systemctl start mediacenter
-      else
-        sudo kill -CONT "-$MC_GPID"
       fi
 
     else
+      # bad mode requested
       continue
     fi
   fi
 done
-
--- resources/script.launch.retropie/addon.py
+++ resources/script.launch.retropie/addon.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os, signal, subprocess, sys, time, xbmc, xbmcgui
+import os, pyxbmct, signal, subprocess, sys, time, xbmc, xbmcgui
 from os import path
 import xml.etree.ElementTree as ET
 from xml.dom import minidom as MD
@@ -63,12 +63,11 @@ def remote_jammer(toggle):
 
 
 # reads a single line from a FIFO
-def readFIFO():
-  global FIFO_PATH
+def read_FIFO(path):
   global MODE
 
   while True:
-    with open(FIFO_PATH) as fifo:
+    with open(path) as fifo:
       for line in fifo:
         line = line.rstrip()
         if line == "NOPADS":
@@ -81,11 +80,126 @@ def readFIFO():
             dialog.ok("Test Exit Buttons", "Did not detect exit combination!")
           exit()
         elif ':' in line:
-          if line.count(':') == 1:
-            return line
+          return line
+        elif line == "0":
+          return line
         elif line == "EXIT":
           return line
 
+
+#
+# class for GUI
+#
+
+class slotManager(pyxbmct.AddonDialogWindow):
+
+  def __init__(self, title=''):
+    super(slotManager, self).__init__(title)
+    self.setGeometry(1280, 720, 9, 10)
+
+    self.slot_btn = []
+    self.del_btn = []
+    self.select_action = []
+    self.delete_action = []
+    self.new_slot_action = self.selectAction(self, 0)
+
+    self.draw_slot_menu()
+    self.map_menu_nav()
+    self.connect(pyxbmct.ACTION_NAV_BACK, self.close)
+
+  def draw_slot_menu(self):
+    # draw and configure select and delete buttons for each slot
+    row = 0
+    while row < slots:
+      platform = labels.pop(0)
+      rom = labels.pop(0)
+
+      self.slot_btn.append(pyxbmct.Button('  %s\n  %s' % (platform, rom), alignment=pyxbmct.ALIGN_LEFT))
+      self.placeControl(self.slot_btn[row], row, 0, 1, 9)
+      self.select_action.append(self.selectAction(self, row+1))
+      self.connect(self.slot_btn[row], self.select_action[row].setTargetSlot)
+
+      self.del_btn.append(pyxbmct.Button('Delete', alignment=pyxbmct.ALIGN_CENTER))
+      self.placeControl(self.del_btn[row], row, 9)
+      self.delete_action.append(self.deleteAction(self, row+1))
+      self.connect(self.del_btn[row], self.delete_action[row].deleteSlot)
+
+      row += 1
+
+    # draw "new session" button if free space (sits in the centre, spread over 2 cells)
+    if slots < MAX_SLOTS:
+      new_row = (((MAX_SLOTS - slots) / 2) + slots)
+      self.new_btn = pyxbmct.Button('Start New Session', alignment=pyxbmct.ALIGN_CENTER)
+      self.placeControl(self.new_btn, new_row, 4, 1, 2)
+      self.connect(self.new_btn, self.new_slot_action.setTargetSlot)
+
+  def map_menu_nav(self):
+    # slot left/right navigation between select and delete columns
+    row = 0
+    while row < slots:
+      self.slot_btn[row].controlLeft(self.del_btn[row])
+      self.slot_btn[row].controlRight(self.del_btn[row])
+      self.del_btn[row].controlLeft(self.slot_btn[row])
+      self.del_btn[row].controlRight(self.slot_btn[row])
+      row += 1
+
+    # up/down navigation for select and delete rows
+    if slots > 1:
+      # upwards
+      row = 1
+      while row < slots:
+        self.slot_btn[row].controlUp(self.slot_btn[row-1])
+        self.del_btn[row].controlUp(self.del_btn[row-1])
+        row += 1
+      # downwards
+      row = 0
+      while row < (slots - 1):
+        self.slot_btn[row].controlDown(self.slot_btn[row+1])
+        self.del_btn[row].controlDown(self.del_btn[row+1])
+        row += 1
+
+    # the "Start New Session" button actually exists and has somewhere to go
+    if slots > 0 and slots < MAX_SLOTS:
+      self.new_btn.controlUp(self.slot_btn[slots-1])
+      self.new_btn.controlDown(self.slot_btn[0])
+      self.slot_btn[0].controlUp(self.new_btn)
+      self.slot_btn[slots-1].controlDown(self.new_btn)
+      self.del_btn[0].controlUp(self.new_btn)
+      self.del_btn[slots-1].controlDown(self.new_btn)
+    elif slots == MAX_SLOTS:
+      self.slot_btn[0].controlUp(self.slot_btn[slots-1])
+      self.slot_btn[slots-1].controlDown(self.slot_btn[0])
+      self.del_btn[0].controlUp(self.del_btn[slots-1])
+      self.del_btn[slots-1].controlDown(self.del_btn[0])
+
+    # Set initial focus
+    if slots > 0:
+      self.setFocus(self.slot_btn[0])
+    else:
+      self.setFocus(self.new_btn)
+
+  # subclasses for GUI button actions
+  class selectAction():
+    def __init__(self, parent, slot):
+      self.parent = parent
+      self.slot = slot
+
+    def setTargetSlot(self):
+      global target_slot
+      target_slot = self.slot
+      self.parent.close()
+
+  class deleteAction():
+    def __init__(self, parent, slot):
+      self.parent = parent
+      self.slot = slot
+
+    def deleteSlot(self):
+      global SWITCHER_FIFO
+      os.system('echo "delete %d" > %s' % (self.slot, SWITCHER_FIFO))
+      xbmc.log("DEBUG: switcher got delete %d > %s" % (self.slot, SWITCHER_FIFO), level=xbmc.LOGNOTICE)
+      self.parent.close()
+      # trigger redraw somehow?
 
 
 #
@@ -100,7 +214,9 @@ HIDDEN="/home/osmc/.kodi/userdata/keymaps/remote.xml.hidden"
 HOBBLE="/home/osmc/.kodi/addons/script.launch.retropie/resources/data/hobble.xml"
 REMOTE="/home/osmc/.kodi/userdata/keymaps/remote.xml"
 SETTINGS="/home/osmc/.kodi/userdata/addon_data/script.launch.retropie/settings.xml"
-FIFO_PATH="/tmp/evdev-exit.fifo"
+EVDEV_FIFO="/tmp/evdev-exit.fifo"
+SWITCHER_FIFO="/tmp/app-switcher.fifo"
+MAX_SLOTS=9
 
 # init dialog handle for all popups
 dialog = xbmcgui.Dialog()
@@ -163,7 +279,38 @@ else:
   except (IOError, AttributeError): # no file or corrupt so leave blank
     xbmc.log("retrOSMCmk2 Launcher: \"%s\" missing or corrupt on this run" % (SETTINGS), level=xbmc.LOGNOTICE)
 
-  # give the scared people something to look at
+
+  if (fast_switching == "true"):
+    # get currently active slots from app-switcher via FIFO
+    os.system('echo "dump" > %s' % (SWITCHER_FIFO))
+    time.sleep(0.1) # don't want to read our own request from the FIFO!
+    msg = read_FIFO(SWITCHER_FIFO)
+
+    # should have received ":" seperated paired list of labels, with num of pairs as first element
+    labels = msg.split(':')
+    slots = int(labels.pop(0))
+    size = len(labels)
+
+    if (slots != size / 2) or (size % 2 != 0):
+      exit() # corrupt data received
+
+    # disable Estuary-based design explicitly
+    pyxbmct.skin.estuary = False # go retro - obviously!
+
+    # display session manager
+    target_slot = -1
+    if __name__ == '__main__':
+      window = slotManager('retrOSMCmk2 - Session Manager')
+      window.doModal()
+      del window
+
+  else:
+    target_slot = 0
+
+  if target_slot == -1:
+    exit() # backed out of session manager without choosing anything
+
+  # give the scared people something to look at whilst Kodi is shutting down
   if (kodi_signals == "false" and fast_switching == "false"):
     xbmc.executebuiltin("ActivateWindow(busydialognocancel)")
 
@@ -177,12 +324,12 @@ else:
 
   # launch Emulationstation +/- fast switching which is needed to block CEC shutdown signals too
   if (kodi_signals == "true" or fast_switching == "true"):
-    os.system('echo "es fast %s" >/tmp/app-switcher.fifo' % (fix_4k))
+    os.system('echo "switch es fast %s %s" >/tmp/app-switcher.fifo' % (target_slot, fix_4k))
   else:
-    os.system('echo "es slow %s" >/tmp/app-switcher.fifo' % (fix_4k))
+    os.system('echo "switch es slow %s" >/tmp/app-switcher.fifo' % (fix_4k))
 
   exit()
-
+  # RetroPie launched - we are gone
 
 # otherwise parse arguments for required mode in settings page
 if INPUTTYPE == "CEC":
@@ -246,8 +393,8 @@ elif INPUTTYPE == "EVDEV":
   # helper will automatically grab the device when testing to avoid a clash with Kodi - no jammer needed here
 
   # ensure FIFO is in place for evdev-helper comms
-  if not path.exists(FIFO_PATH):
-    os.mkfifo(FIFO_PATH)
+  if not path.exists(EVDEV_FIFO):
+    os.mkfifo(EVDEV_FIFO)
 
   if MODE == "PROGRAM":
     dialog.textviewer("Program Exit Buttons", "These will work like RetroPie.\n\nYou configure a hotkey enable button and an exit button.  For example to exit back to EMulationstation most people are configured to hold down \"select\" and press \"start\".\n\nThe enable button could be the same as RetroPie, but the switch button MUST NOT ALREADY BE ASSIGNED to anything else in RetroPie e.g. exit, reset, save/load gamestate.\n\nProgramming instructions\n1. Press OK\n2. When requested press the hotkey enable button on the gamepad.\n3. Then when requested press the gamepad button you will use for the switching function.")
@@ -255,7 +402,7 @@ elif INPUTTYPE == "EVDEV":
     # collect controller name and hotkey enable button
     subprocess.Popen([EVHELPER, "SCANMULTI"])
     dialog.notification("Program Exit Buttons", "Press hotkey enable button...", xbmcgui.NOTIFICATION_INFO, 3000)
-    msg = readFIFO()
+    msg = read_FIFO(EVDEV_FIFO)
     # validate output = gamepad-id:button-id
     gamepad = msg.split(':', 1)[0]
     hotbtncode = msg.split(':', 1)[1]
@@ -265,12 +412,12 @@ elif INPUTTYPE == "EVDEV":
     # collect exit button for the same controller
     subprocess.Popen([EVHELPER, "SCANSINGLE", gamepad, "PARENT"])
     dialog.notification("Program Exit Buttons", "Press exit button...", xbmcgui.NOTIFICATION_INFO, 3000)
-    msg = readFIFO()
+    msg = read_FIFO(EVDEV_FIFO)
     # validate output - gamepad id, button id
     if not msg.split(':', 1)[0] == gamepad:
       dialog.ok("Program Exit Buttons", "\"Exit Button\" must be on the same controller as the \"Hotkey Enable Button\"!")
       exit()
-  
+
     exitbtncode = msg.split(':', 1)[1]
     # exit button cannot be the same as the hotkey enable button...
     if exitbtncode == hotbtncode:
@@ -302,7 +449,7 @@ elif INPUTTYPE == "EVDEV":
       # test exit button combination
       subprocess.Popen([EVHELPER, "CATCHCOMBO", "TEST", gamepad, hotbtncode, exitbtncode])
       dialog.notification("Testing Exit Buttons", "Press exit combination...", xbmcgui.NOTIFICATION_INFO, 3000)
-      msg = readFIFO()
+      msg = read_FIFO(EVDEV_FIFO)
       if msg == "EXIT":
         dialog.ok("Test Exit Buttons", "Exit combination detected correctly!")
       elif msg == "TIMEOUT":
@@ -313,3 +460,4 @@ elif INPUTTYPE == "EVDEV":
 
 else:
   xbmc.log("ERROR!\n\"%s\" is a bad argument for %s" % (INPUTTYPE, sys.argv[0]), level=xbmc.LOGNOTICE)
+#  xbmc.log("DEBUG: ", level=xbmc.LOGNOTICE)
